c615bcaf2bb6cf09403590ac8ce54d11a09845ec
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2023-11-19 21:58:17 +0545

    Mongodb database setup

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js'
 import { notFound, errorHandler } from './Middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 dotenv.config();
+connectDB();
 
 const app = express();
-
 app.use('/api/users',userRoutes);
 
 //custom error handling 
