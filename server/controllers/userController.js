bc2840e2c4d5d20d30d9c730de23cb1034bacfb0
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2023-11-21 06:31:47 +0545

    generate jwt and set cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
-
+import generateToken from '../utils/generateToken.js';
 //@des      Auth user/set Token
 // @route   POST /api/users/auth
 // @access  public
@@ -32,6 +32,7 @@ export const registerUser = async (req, res) => {
     });
 
     if (user) {
+        generateToken(res,user._id);
       res.status(200).json({ _id: user._id, name: user.name, email: user.email });
     } else {
       res.status(400);
---
+++ server/utils/generateToken.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userid) => {
+  const token = jwt.sign({ userid }, process.env.JWT_SECRET, {
+    expiresIn: '60s',
+  });
+  maxAge: 60;
+  // Now we need to save it in a cookie
+  res.cookie('jwt', token, [
+    {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 60,  // Set maxAge to match the expiration time in seconds
+    },
+  ]);
+};
+
+export default generateToken;
